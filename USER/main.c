241dfdce32829e3f57a6cdbff5121b2ec1d06f43
Smallmagua <1474341998@qq.com>
2023-11-01 14:59:53 +0800

    第一次提交

---
+++ USER/main.c
@@ -0,0 +1,56 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "usart.h"
+ 
+/************************************************
+ ALIENTEK精英STM32开发板实验4
+ 串口 实验   
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+void GPIO_LED(void);
+ int main(void)
+ {		
+ 	u16 t;  
+	u16 len;	
+
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	uart_init(9600);	 
+ 	printf("共享停车位\r\n");
+ 	while(1)
+	{
+		if(USART_RX_BUF[0] == '1')
+			{
+				printf("OK\r\n");
+				GPIO_ResetBits(GPIOB,GPIO_Pin_13);
+			}
+		else if(USART_RX_BUF[0] == '2')
+		{ 
+			GPIO_SetBits(GPIOB,GPIO_Pin_13);
+		}			
+	}	
+ }
+ 
+ void GPIO_LED(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  
+  RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB , ENABLE); 						 
+//=============================================================================
+//LED -> PB13
+//=============================================================================			 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_15);
+}
+
