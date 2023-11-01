c00357afeda9193168208bba95e1029bc050ecd6
Smallmagua <1474341998@qq.com>
2023-11-01 15:23:11 +0800

    修改GPIO引脚

--- USER/main.c
+++ USER/main.c
@@ -45,7 +45,7 @@ void GPIO_LED(void);
 //=============================================================================
 //LED -> PB13
 //=============================================================================			 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
   GPIO_Init(GPIOB, &GPIO_InitStructure);
